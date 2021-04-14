76d6631ae8ed5769a369d47b03c7bce2bfb994e4
mohammadroman <78169792+mohammadroman@users.noreply.github.com>
2021-04-14 09:04:37 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const openside = document.querySelector(".openside");
+const sidebarMenu = document.querySelector('.sidebarMenu');
+
+
+
+
+    openside.addEventListener('click',function() {
+
+
+
+        sidebarMenu.classList.toggle('sidebarclick');
+    
+    
+    });
+
+
+    const openPage2 = document.querySelector(".openPage2");
+    const sidebarMenu2 = document.querySelector(".sidebarMenu2");
+
+
+    openPage2.addEventListener('click',function() {
+
+      openPage2.classList.toggle('.ul-sidebar22');
+
+
+    })
+
+      
+
+  
+    
